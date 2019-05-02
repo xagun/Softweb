6ddade7d7fef74c87c62986662293a85247dc89f
[xagun] <[zeekes.smile@gmail.com]>
2019-05-02 08:27:49 +0545

    applicatoion server initialized

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('index is running');
+
+var express = require('express');
+
+var myapplication = new express();
+
+myapplication.listen(3001);
+
+// var myapplication2 = new express();
+// myapplication2.listen(3002);
